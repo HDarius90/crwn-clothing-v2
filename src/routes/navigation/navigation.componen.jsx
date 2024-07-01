cf5a9370a33630a69e50d2b5354f5f11134cdb90
Darius <darius.hadjati@gmail.com>
2024-07-01 17:57:38 +0200

    creating basic routing structure and navigation component

---
+++ src/routes/navigation/navigation.componen.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'react-router-dom';
+import './navigation.styles.scss';
+import { ReactComponent as CrwLogo } from '../../assets/crown.svg';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
