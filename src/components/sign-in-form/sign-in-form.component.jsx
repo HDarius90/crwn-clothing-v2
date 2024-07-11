e47fe40ec895a64927334dea12d618513cc14d6c
Hadjati Darius <darius.hadjati@gmail.com>
2024-07-11 19:00:39 +0200

    Finishing authentication page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -38,10 +38,15 @@ const SignInForm = () => {
       console.log(response);
       resetFormFields();
     } catch (error) {
-      if (error.code === "auth/invalid-credential") {
-        alert("Invalid email or password");
-      } else {
-        console.log(error.code, error.message);
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
       }
     }
   };
@@ -75,7 +80,11 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign-In</Button>
-          <Button buttonType={"google"} onClick={signInWithGoogle}>
+          <Button
+            type="button"
+            buttonType="google"
+            onClick={signInWithGoogle}
+          >
             Google Sign In
           </Button>
         </div>
