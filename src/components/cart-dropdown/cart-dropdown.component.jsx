b097e7476122fcf2b8513f7e15dc2de4a2d5532e
Darius Hadjati <darius.hadjati@gmail.com>
2024-10-11 23:47:05 +0200

    Migrate Cart Context to Redux

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,18 @@
-import { useContext } from 'react';
-
-import { CartContext } from '../../contexts/cart.context';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../store/cart/cart.selector';
 
 import {
   CartDropdownContainer,
   CartItems,
   EmptyMessage,
 } from './cart-dropdown.styles';
-import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
