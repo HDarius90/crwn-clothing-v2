3602f6f5cba82dc0038bcda4bbd3598fe57d1e65
Darius Hadjati <darius.hadjati@gmail.com>
2024-10-17 21:14:18 +0200

    impl login middleware + install redux-persist

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
