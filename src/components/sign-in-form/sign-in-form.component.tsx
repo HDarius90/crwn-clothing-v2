37692a97e3709843d4d69d87561c3219c30a31bc
Hadjati Darius <darius.hadjati@gmail.com>
2024-12-06 14:41:05 +0100

    Typing Input & Form Components

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
 import { useDispatch } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
@@ -30,8 +30,9 @@ const SignInForm = () => {
     dispatch(googleSignInStart(navigate));
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     try {
       dispatch(emailSignInStart(email, password, navigate));
       resetFormFields();
@@ -40,7 +41,7 @@ const SignInForm = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
