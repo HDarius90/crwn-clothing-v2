cc4e88924a686da5d7781d60d928f651c2dba1af
Hadjati Darius <darius.hadjati@gmail.com>
2024-12-11 12:09:06 +0100

    Typing SVG Imports

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -35,7 +35,7 @@ const Navigation = () => {
         <NavLinks>
           <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <NavLink as="span" onClick={signOutUser}>
+            <NavLink as="span" to="/" onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
