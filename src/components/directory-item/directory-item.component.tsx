79b060c0b67bec22047c8a1f63d3715209a34e19
Hadjati Darius <darius.hadjati@gmail.com>
2024-12-17 12:55:00 +0100

    fix: DirectoryItem

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,17 +1,16 @@
+import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { DirectoryCategory } from '../directory/directory.component.js';
 
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
-} from './directory-item.styles.jsx';
-import { FC } from 'react';
-import { Category } from '../../store/categories/category.types.js';
+} from './directory-item.styles';
 
 type DirectoryItemProps = {
-  category: Category & {
-    route: string;
-  };
+  category: DirectoryCategory;
 };
 
 const DirectoryItem: FC<DirectoryItemProps> = ({ category }) => {
